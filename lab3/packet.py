e2fbcc5f1f192bf1676aae49e095c4856c6d8ecc
stanleyshen2003 <stanley.shen2003@gmail.com>
2025-07-29 00:16:12 +0800

    add py

---
+++ lab3/packet.py
@@ -0,0 +1,16 @@
+from scapy.all import sniff, BOOTP, DHCP
+
+def dhcp_monitor(pkt):
+    if DHCP in pkt and BOOTP in pkt:
+        dhcp_message_type = pkt[DHCP].options[0][1]
+        msg_types = {
+            1: "Discover",
+            2: "Offer",
+            3: "Request",
+            5: "ACK"
+        }
+        print(f"[+] DHCP {msg_types.get(dhcp_message_type, 'Other')} detected")
+        print(pkt.summary())
+
+# Sniff DHCP packets on your interface (e.g., "eth0")
+sniff(filter="udp and (port 67 or port 68)", prn=dhcp_monitor, store=0)
